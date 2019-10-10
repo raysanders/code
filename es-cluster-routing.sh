d6d42d3c49ff4c318edc85a6334768ecfcf46ec6
Ray Sanders <rjs.devops@raymondjsanders.com>
2019-10-10 14:41:54 -0700

    Delete es-cluster-routing.sh

--- es-cluster-routing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Created by Ray Sanders
-# this script will force es to behave better in regards to cluster allocation.
-curl -XPUT localhost:9200/_cluster/settings -d '{"transient": {"cluster.routing.allocation.allow_rebalance" :"'always'"}}'
-curl -XPUT localhost:9200/_cluster/settings -d '{"transient": {"cluster.routing.allocation.cluster_concurrent_rebalance" :"'10'"}}'
-curl -XPUT localhost:9200/_cluster/settings -d '{"transient": {"cluster.routing.allocation.cluster_concurrent_recoveries" :"'10'"}}'
-curl -XPUT localhost:9200/_cluster/settings -d '{"transient": {"cluster.routing.allocation.enable" :"'all'"}}'
