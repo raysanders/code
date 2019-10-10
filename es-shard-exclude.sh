d072de40621f0d1e421c27624fa98446f32bdb70
Ray Sanders <rjs.devops@raymondjsanders.com>
2019-10-10 14:42:08 -0700

    Delete es-shard-exclude.sh

--- es-shard-exclude.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Created by Ray Sanders
-# this script will exclude elasticsearch nodes
-curl -XPUT localhost:9200/_cluster/settings -d '{"transient": {"cluster.routing.allocation.exclude._id" :"'$1'"}}'
