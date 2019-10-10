4141855fed83d681f8e22781c4ebc1e5dd21551c
Ray Sanders <rjs.devops@raymondjsanders.com>
2019-10-10 14:42:45 -0700

    Delete es-shard-reallocate.sh

--- es-shard-reallocate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Created by Ray Sanders
-# this script will re-assign elasticsearch shards
-# replace INDEXNAME, X, and NODE with the name of the index with missing/unallocated shards, 
-# X with the unallocated shard #, and NODE with the ES node you wish to allocate the shard to. 
-curl -X POST     'http://localhost:9200/_cluster/reroute?pretty=true'     -d '{ "commands" : [ { "allocate" : { "index" : "INDEXNAME", "shard" : X , "node" : "NODE", "allow_primary" : 1 }}]}'
